471486fc7e37305789e93748e0983c324bb82e8a
troptaz-netizen <troptaz@gmail.com>
2026-04-09 12:36:33 +0100

    Forgot to add provider in example

--- example/game/game.gd
+++ example/game/game.gd
@@ -4,4 +4,4 @@ func _ready() -> void:
 	$Button.pressed.connect(_on_button_pressed)
 
 func _on_button_pressed() -> void:
-	get_parent().get_value().navigate_to_menu()
+	Provider.of_type(NavigationStateMachine, self).navigate_to_menu()
